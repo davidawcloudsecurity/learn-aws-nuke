a25857af097b38b4aa82cb8ff94e62523afe676e
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-06-13 15:31:52 +0800

    Update del_s3.sh - added versioning

--- del_s3.sh
+++ del_s3.sh
@@ -17,6 +17,22 @@ delete_objects_in_bucket() {
     echo "All objects in bucket $bucket_name have been deleted."
 }
 
+delete_version_in_bucket () {
+bucket_name=$1
+
+# Get list of object versions
+object_versions=$(aws s3api list-object-versions --bucket "$bucket_name" --output=json --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}')
+
+# Loop through each object version and delete it
+echo "$object_versions" | jq -c '.Objects[]' | while IFS= read -r object; do
+    key=$(echo "$object" | jq -r '.Key')
+    versionId=$(echo "$object" | jq -r '.VersionId')
+    echo "Deleting object version: $key (VersionId: $versionId)"
+    aws s3api delete-object --bucket "$bucket_name" --key "$key" --version-id "$versionId"
+done
+aws s3 rb s3://$bucket_name --force
+}
+
 # Get the list of all S3 buckets
 echo "Fetching list of S3 buckets..."
 buckets=$(aws s3api list-buckets --query "Buckets[].Name" --output text)
@@ -26,7 +42,7 @@ for bucket in $buckets; do
     #delete_objects_in_bucket $bucket
     aws s3 rb s3://$bucket --force
     aws s3api delete-bucket --bucket $bucket --region ap-southeast-1
-
+    delete_version_in_bucket $bucket
 done
 
 echo "All S3 buckets and their objects have been deleted."
