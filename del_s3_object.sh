e150690857d9a59151bf78b50ebad930d3b0deba
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-06-15 17:35:32 +0800

    Update del_s3_object.sh

--- del_s3_object.sh
+++ del_s3_object.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ask for user input
-read -p "Enter the resource type you want to nuke: " bucket_name
+read -p "Enter the bucket name you want to empty: " bucket_name
 
 # Get delete markers and versions
 delete_markers=$(aws s3api list-object-versions --bucket "$bucket_name" --query DeleteMarkers[*].Key --output json)
@@ -13,9 +13,9 @@ delete_objects() {
     local version_ids="$2"
     local bucket_name="$3"  # Replace with your actual bucket name
 
-#    count=$(echo "$keys" | jq length)
-    for ((i=0;i < 2;i++)); do
-        key=$(echo "$keys" | jq -r ".[$i]")
+    count=$(echo "$keys" | jq length)
+    for ((i=0;i < count;i++)); do
+       key=$(echo "$keys" | jq -r ".[$i]")
         version_id=$(echo "$version_ids" | jq -r ".[$i]")
         echo "Deleting object with key: $key and version ID: $version_id"
         aws s3api delete-object --bucket "$bucket_name" --key "$key" --version-id "$version_id"
@@ -24,5 +24,5 @@ delete_objects() {
 
 # Delete delete markers
 delete_objects "$delete_markers" "$versions" "$bucket_name"
-
+aws s3 rb s3://$bucket_name --force
 echo "All delete markers and versions deleted from bucket $bucket_name."
