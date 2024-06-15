45b33f79df82c47640e45bc79cfda0a9b5dffdb1
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-06-15 18:25:04 +0800

    Update del_s3_jq.sh

--- del_s3_jq.sh
+++ del_s3_jq.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Ask for user input
+read -p "Enter the bucket name you want to empty: " bucket_name
+
+aws s3api put-bucket-versioning --bucket "$bucket_name" --versioning-configuration Status=Suspended
 # Pipe the output to a while loop to delete objects in batches
-aws s3api list-object-versions --bucket "wifi-counter-sensor-6oq20d9" --output=json | \
+aws s3api list-object-versions --bucket "$bucket_name" --output=json | \
   jq -r '.Versions[] | {Key: .Key, VersionId: .VersionId} | @base64' | \
   while IFS= read -r line; do
     # Decode the line from base64
