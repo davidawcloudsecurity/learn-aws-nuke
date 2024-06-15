569036a6cc9f00b71c9dd486ef34f46e7438ee63
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-06-15 17:28:59 +0800

    Update del_s3.sh - introduce object delete

--- del_s3.sh
+++ del_s3.sh
@@ -5,31 +5,17 @@ empty_s3_bucket() {
     bucket_name=$1
     echo "Emptying bucket: $bucket_name"
     
-    # List and delete all object versions (to handle versioned buckets)
-    aws s3api list-object-versions --bucket $bucket_name  --query DeleteMarkers[*].Key --output text
-    object_keys=$(aws s3api list-object-versions --bucket $bucket_name  --query DeleteMarkers[*].Key --output text)
-    version_ids=$(aws s3api list-object-versions --bucket $bucket_name  --query DeleteMarkers[*].VersionId --output text)
-    for (( i=0; i<${#object_keys[@]}; i++ )); do
-      object_key="${object_keys[$i]}"
-      version_id="${version_ids[$i]}"
-      echo "Deleting object with key: $object_key and version ID: $version_id"
-      aws s3api delete-object --bucket "$bucket_name" --key "$object_key" --version-id "$version_id"
+    # Get delete markers and versions
+    Keys=$(aws s3api list-object-versions --bucket "$bucket_name" --query DeleteMarkers[*].Key --output json)
+    versions=$(aws s3api list-object-versions --bucket "$bucket_name" --query DeleteMarkers[*].VersionId --output json)
+
+    count=$(echo "$Keys" | jq length)
+    for ((i=0;i < count;i++)); do
+        key=$(echo "$Keys" | jq -r ".[$i]")
+        version_id=$(echo "$versions" | jq -r ".[$i]")
+        echo "Deleting object with key: $key and version ID: $version_id"
+        aws s3api delete-object --bucket "$bucket_name" --key "$key" --version-id "$version_id"
     done
-
-    # List and delete all delete markers (to handle versioned buckets)
-    delete_markers=$(aws s3api list-object-versions --bucket "$bucket_name" --query '{Objects: DeleteMarkers[].{Key:Key,VersionId:VersionId}}' --output json)
-    if [[ "$delete_markers" != "{}" ]]; then
-        echo "Deleting delete markers in bucket: $bucket_name"
-        aws s3api delete-objects --bucket "$bucket_name" --delete "$delete_markers"
-    fi
-
-    # List and delete all objects (for non-versioned buckets)
-    objects=$(aws s3api list-objects --bucket "$bucket_name" --query '{Objects: Contents[].{Key:Key}}' --output json)
-    if [[ "$objects" != "{}" ]]; then
-        echo "Deleting objects in bucket: $bucket_name"
-        aws s3api delete-objects --bucket "$bucket_name" --delete "$objects"
-    fi
-
     echo "Bucket $bucket_name is now empty."
 }
 
